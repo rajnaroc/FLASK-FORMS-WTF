adf576034ce592563ad60701ea3a77af9955a58b
Aaron Campos <aaroncm611@gmail.com>
2025-02-04 14:22:14 +0100

    formulario acabada

---
+++ src/form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired,Length
+
+class Persona(FlaskForm):
+    username = StringField('Username', validators=[
+        DataRequired(),
+        Length(min=3, max=10)     
+        ])
+    email = StringField('Email', validators=[
+        DataRequired(),
+        Length(min=5, max=12)
+    ])
+    telefono = StringField('Telefono', validators=[
+        DataRequired(),
+        Length(min=9, max=14)
+    ])
+    password = StringField('Password', validators=[
+        DataRequired(),
+        Length(min=4, max=8)
+    ])
+    submit = SubmitField('Enviar')
